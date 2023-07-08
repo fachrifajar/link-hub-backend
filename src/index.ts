f6c2e175f090aadf0e5c8d33a3e3b75e37e9df59
master <fachri.fajar@gmail.com>
2023-07-08 10:42:03 +0700

    [fix] cors-3

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ app.use(
   cors({
     origin: ["http://localhost:3000", "https://link-hub-v1.vercel.app"],
     credentials: true,
+    exposedHeaders: ["Set-Cookie"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
